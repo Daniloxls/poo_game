0bc27c416b0fe92407f34109f6e97f413e9f480b
Danilo-Danilo-Danilo <danilo.lacerda@aluno.ufca.edu.br>
2024-04-25 19:43:20 -0300

    bug-fixes

--- scenes/overworld/scripts/overworld_south.gd
+++ scenes/overworld/scripts/overworld_south.gd
@@ -15,12 +15,12 @@ func _process(delta):
 	pass
 
 func starting_cutscene():
-	#player.set_in_scene(true)
-	#var tween = create_tween()
-	#tween.tween_callback(player.set_sprite.bind("idle_left"))
-	#tween.tween_callback(kath.set_sprite.bind("idle_right"))
-	#tween.tween_callback(kath.play_animation.bind("jump"))
-	#tween.tween_callback(textbox.queue_text.bind(["Turin seu boboca", "Você dormiu demais de novo ?", "A gente tinha combinado de ir pro festival da colheita hoje", "Vamos, ainda dá tempo de ver a atração principal", "O ultimo que chegar compra uma fatia de torta pro vencedor!"]))
+	player.set_in_scene(true)
+	var tween = create_tween()
+	tween.tween_callback(player.set_sprite.bind("idle_left"))
+	tween.tween_callback(kath.set_sprite.bind("idle_right"))
+	tween.tween_callback(kath.play_animation.bind("jump"))
+	tween.tween_callback(textbox.queue_text.bind(["Turin seu boboca", "Você dormiu demais de novo ?", "A gente tinha combinado de ir pro festival da colheita hoje", "Vamos, ainda dá tempo de ver a atração principal", "O ultimo que chegar compra uma fatia de torta pro vencedor!"]))
 	pass
 
 
@@ -28,10 +28,10 @@ func _on_textbox_text_finish():
 	var tween = create_tween()
 	if kath.get_seq() == 0:
 		tween.tween_callback(kath.set_sprite.bind("walk_left"))
-		tween.tween_property(kath, "position", Vector2(6400,7417), 1.6).set_trans(Tween.TRANS_LINEAR)
+		tween.tween_property(kath, "position", Vector2(6000,-4500), 1.6).set_trans(Tween.TRANS_LINEAR)
 		tween.tween_callback(kath.set_sprite.bind("walk_up"))
-		tween.tween_property(kath, "position", Vector2(6400,4000), 1.6).set_trans(Tween.TRANS_LINEAR)
+		tween.tween_property(kath, "position", Vector2(6000,-8000), 1.6).set_trans(Tween.TRANS_LINEAR)
 		tween.tween_callback(player.set_in_scene.bind(false))
-		tween.tween_callback(kath.set_position.bind(Vector2(4500, -5000)))
+		tween.tween_callback(kath.set_position.bind(Vector2(5200, -10300)))
 		tween.tween_callback(kath.set_sprite.bind("idle_down"))
 		kath.set_seq(1)
